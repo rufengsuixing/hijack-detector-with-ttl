5d97719efbf741db67d5d42e1b3aa08f6b353834
rufengsuixing001 <1985151814@qq.com>
2018-03-04 22:53:45 +0800

    。

--- det.py
+++ det.py
@@ -1,10 +1,10 @@
 '''
 author:rufengsuixing
-'''
-from scapy.all import *
-from scapy_http import http
+ '''
+from scapy3k.all import *
+from scapy3k.layers import http
 conf.prog.wireshark='C:/Program Files/Wireshark/Wireshark.exe'
-
+import time
 
 def ttl_cul(ps,latitude=4,debug=False):
     ip_arr = {}
@@ -56,8 +56,123 @@ def ttl_cul(ps,latitude=4,debug=False):
                 ttl_show_time[b]=1
         '''
     return ttl_show_time
+def pkfil_cul(samepk,notsamepk):
+    tar=samepk[0].payload.original
+    chong=set()
+    for b in notsamepk:
+        bi=b.payload.original
+        for suo in range(0,200):
+            if suo in chong:
+                continue
+            try:
+                if bi[suo:suo+4]==tar[suo:suo+4]:
+                    chong.add(suo)
+            except IndexError:
+                pass
+    for x in samepk[1:]:
+        bi=x.payload.original
+        for suo in range(0,200):
+            if suo in chong:
+                continue
+            try:
+                if bi[suo:suo+4]!=tar[suo:suo+4]:
+                    chong.add(suo)
+            except IndexError:
+                pass
+    for a in range(0,200):
+        if a not in chong:
+            print(a,' ',tar[a:a+4])
+    print('以上为可作为判断准则的字段和值')
+def resp_cul(ps,xiangsidu=0.9):
+    #用来寻找js劫持，套壳相似度很高
+    import Levenshtein
+    
+    ip_arr = {}
+    for b in ps:
+        if b[http.HTTPResponse].fields['Status-Line'].find(b'200')==-1:
+            continue
+        try:
+            ct=b[http.HTTPResponse].fields['Content-Type']
+            if ct.find(b'text')==-1:
+                if ct.find(b'javascript')==-1:
+                    continue
+            if b[http.HTTPResponse].fields['Content-Length']==b'0':
+                continue
+        except Exception as e:
+            print(e)
+            continue
+        try:
+            if b[http.HTTPResponse].fields['Content-Encoding'].find(b'gzip')!=-1:
+                continue
+        except:
+            pass
+        try:    
+            charset=re.findall(b'charset=(.*);*',ct)[0].decode()
+        except:
+            charset='utf-8'
+        ip_src = b.sprintf(r"%IP.src%")    
+        try:
+            ip_resp = b[http.HTTPResponse].original.decode(encoding=charset,errors='strict')
+        except Exception as e:
+            #print(e)
+            try:
+                ip_resp = b[http.HTTPResponse].original.decode(encoding='gb2312',errors='strict')
+            except:
+                continue
+        ps[2]
+        if ip_src in ip_arr:
+            ip_arr[ip_src][ip_resp]=b
+        else:
+            ip_arr[ip_src]={ip_resp:b}
+    xiangsi=[]
+    disf=1000
+    kz=0
+    for b in ip_arr:
+        kz+=1
+        tmp=kz
+        for c in ip_arr:
+            if tmp>0:
+                tmp-=1
+                continue
+            for d in ip_arr[b]:
+                for e in ip_arr[c]:
+                    '''
+                    #第一种方法
+                    #dis=Levenshtein.distance(d, e)
+                    print(dis,end=' ')
+                    if dis<150:
+                        xiangsi.add(d)
+                        xiangsi.add(e)
+                    elif disf>dis:
+                        disf=dis
+                        noxiangsi=[d,e]
+                    #第二种方法
+                    dis=Levenshtein.jaro(d,e)
+                    print(dis,end=' ')
+                    if dis>0.8:
+                        xiangsi.add(d)
+                        xiangsi.add(e)
+                    '''
+                    #自己的方法
+                    lid=d.split('\n')
+                    lie=e.split('\n')
+                    si=0
+                    for a in range(0,len(lid)):
+                        try:
+                            if lid[a].replace('\r','')==lie[a].replace('\r',''):
+                                si+=1
+                        except Exception as e:
+                            a-=1
+                            pass
+                    xiang=si/(a+1)
+                    #print(xiang)
+                    if xiang>xiangsidu:
+                        xiangsi.append(ip_arr[b][d])
+                        xiangsi.append(ip_arr[c][e])
+    #print(xiangsi)
+    pkfil_cul(xiangsi,[a for a in ps if a not in xiangsi])
 
-def det_ttl(latitude=4,count=500,debug=False):
+def det_ttl(latitude=4,count=100,debug=False):
     def pcap_fil(p):
         if http.HTTPResponse in p:
             return True
@@ -68,25 +183,22 @@ def det_ttl(latitude=4,count=500,debug=False):
     print ("ipv4 http responce Hijacking Delector by rufengsuixing with ttl，请关闭可能导致重置ttl的设备，如路由器广告过滤")
     print ("Sniffing .... please open many http pages and i will tell you what ttl most like hijack")
     if debug:
-        a=sniff( filter="tcp port 80",lfilter=pcap_fil,offline='test.pcap')
+        a=sniff(lfilter=pcap_fil,count=500,offline='respont.pcap')
+        #a=sniff(offline='temp.cap')
     else:
-        a=sniff( filter="tcp port 80",lfilter=pcap_fil, count=count)
+        a=sniff( filter="tcp port 80",lfilter=pcap_fil,count=count)
     ttl_show_time=ttl_cul(a,latitude=latitude,debug=debug)
-
     if not ttl_show_time:
         print('ttl detect failed ,maybe isp modified it')
         return None
     else:
         print('result:',ttl_show_time)
         return a
-def det_http_resp(ps):
-    for a in ps:
-        ip_src = b.sprintf(r"%IP.src%")
-        a[http.HTTPResponse]
 
 def creat_ipt(ttl):
     print('''for your router
-             iptables -I FORWARD -p tcp --sport 80 -m ttl --ttl %d -j DROP
+             iptables -I INPUT -p tcp --sport 80 -m ttl --ttl %d -j DROP
              iptables -I FORWARD -p tcp --sport 80 -m ttl --ttl %d -j DROP'''%(ttl,ttl+1))
 if __name__=='__main__':
-    det_ttl(debug=False)
+    a=det_ttl(debug=False)
+    resp_cul(a)
